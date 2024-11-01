69a0360d1726d596c178e6a07243a2fdf474888b
TopScrech <sergei_saliukov@icloud.com>
2024-11-01 22:40:12 +0100

    Initial Commit

---
+++ Glucosy/GlucosyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucosyApp.swift
+//  Glucosy
+//
+//  Created by Sergei Saliukov on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GlucosyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

344b1616f941aef5292b16b8df2acbe9919e1bf8
TopScrech <sergei_saliukov@icloud.com>
2025-01-08 11:57:16 +0100

    -

--- Glucosy/Records/Carbs/Carbs List.swift
+++ Glucosy/Records/Carbs/Carbs List.swift
@@ -1,12 +1,22 @@
-import SwiftUI
+import ScrechKit
 
 struct CarbsList: View {
     @Environment(HealthKit.self) private var vm
+    
+    @State private var sheetNewRecord = false
     
     var body: some View {
         List {
             ForEach(vm.carbsRecords) { record in
                 CarbsCard(record)
+            }
+        }
+        .sheet($sheetNewRecord) {
+            NewRecordSheet()
+        }
+        .toolbar {
+            SFButton("note.text.badge.plus") {
+                sheetNewRecord = true
             }
         }
     }
--- Glucosy/Records/Glucose/Glucose List.swift
+++ Glucosy/Records/Glucose/Glucose List.swift
@@ -1,12 +1,22 @@
-import SwiftUI
+import ScrechKit
 
 struct GlucoseList: View {
     @Environment(HealthKit.self) private var vm
+    
+    @State private var sheetNewRecord = false
     
     var body: some View {
         List {
             ForEach(vm.glucoseRecords) { record in
                 GlucoseCard(record)
+            }
+        }
+        .sheet($sheetNewRecord) {
+            NewRecordSheet()
+        }
+        .toolbar {
+            SFButton("note.text.badge.plus") {
+                sheetNewRecord = true
             }
         }
     }

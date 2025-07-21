0f3819a62fe2fd333c974c74b4669b209d2e7a30
TopScrech <sergei_saliukov@icloud.com>
2025-07-21 12:25:08 +0200

    protocol HealthRecord

---
+++ iOS/Records/HealthKit/Models/Health Record.swift
@@ -0,0 +1,44 @@
+import HealthKit
+
+protocol HealthRecord: Identifiable {
+    var id: UUID                 { get }
+    var value: Double            { get }
+    var sample: HKQuantitySample { get }
+    var date: Date               { get }
+    var source: String           { get }
+}
+
+extension HealthRecord {
+    var date: Date {
+        sample.startDate
+    }
+    
+    var source: String {
+        "\(sample.sourceRevision.source.name) \(sample.sourceRevision.source.bundleIdentifier)"
+    }
+}
+
+struct Carbohydrates: HealthRecord {
+    let id = UUID()
+    let value: Double
+    let sample: HKQuantitySample
+}
+
+struct Glucose: HealthRecord {
+    let id = UUID()
+    let value: Double
+    let sample: HKQuantitySample
+}
+
+struct Insulin: HealthRecord {
+    let id = UUID()
+    let value: Double
+    let type: InsulinType
+    let sample: HKQuantitySample
+    
+    var formattedValue: String {
+        value.truncatingRemainder(dividingBy: 1) == 0 ?
+        String(format: "%.0f", value) :
+        String(value)
+    }
+}

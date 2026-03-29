53666307233ee0eab81c85262daa4d2bed401e70
TopScrech <sergei_saliukov@icloud.com>
2026-03-29 23:49:34 +0200

    -

--- iOS/Nav/HomeAction.swift
+++ iOS/Nav/HomeAction.swift
@@ -2,16 +2,17 @@ import SwiftUI
 
 enum HomeAction {
     case startNovoPenScan
-
+    
     init?(shortcutItem: UIApplicationShortcutItem) {
         switch shortcutItem.type {
         case Self.startNovoPenScanType:
             self = .startNovoPenScan
+            
         default:
             return nil
         }
     }
-
+    
     private static var startNovoPenScanType: String {
         "\(Bundle.main.bundleIdentifier ?? "dev.topscrech.Glucosy").startNovoPenScan"
     }

021b1896c20f5f10e62933db068ea6a719006a94
TopScrech <sergei_saliukov@icloud.com>
2026-04-05 17:29:00 +0200

    Created visionOS target #2

---
+++ visionOS/visionApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionApp.swift
+//  vision
+//
+//  Created by Sergei Saliukov on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct visionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

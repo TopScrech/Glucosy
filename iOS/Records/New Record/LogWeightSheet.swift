0013ca15b2e38c2e47bb116f23c5a0054cbd98a3
Sergei Saliukov <sergei_saliukov@icloud.com>
2026-04-23 08:23:25 +0200

    iOS 17 support

--- iOS/Records/New Record/LogWeightSheet.swift
+++ iOS/Records/New Record/LogWeightSheet.swift
@@ -4,28 +4,76 @@ struct LogWeightSheet: View {
     @Environment(HealthKit.self) private var vm
     @Environment(\.dismiss) private var dismiss
     
+    @State private var date = Date()
     @State private var selectedValue = 64
+    @State private var enteredWeight = ""
+    @FocusState private var isWeightFieldFocused: Bool
+    
+    private var fallbackWeight: Double? {
+        Double(enteredWeight.replacing(",", with: "."))
+    }
     
     var body: some View {
-        VStack {
-            WheelPickerView(range: 15...150, selectedValue: $selectedValue) { currentValue in
-                VStack {
-                    Text(String(currentValue))
-                        .monospacedDigit()
-                        .largeTitle(.black, design: .rounded)
-                        .numericTransition()
-                        .animation(.snappy, value: currentValue)
+        Group {
+            if #available(iOS 18, *) {
+                List {
+                    Section {
+                        DatePicker("Date", selection: $date, displayedComponents: .date)
+                            .secondary()
+                        
+                        DatePicker("Time", selection: $date, displayedComponents: .hourAndMinute)
+                            .secondary()
+                    }
                     
-                    Text("KG")
-                        .callout()
-                        .foregroundStyle(.gray)
+                    Section {
+                        WheelPickerView(range: 15...150, selectedValue: $selectedValue) { currentValue in
+                            VStack {
+                                Text(String(currentValue))
+                                    .monospacedDigit()
+                                    .largeTitle(.black, design: .rounded)
+                                    .numericTransition()
+                                    .animation(.snappy, value: currentValue)
+                                
+                                Text("KG")
+                                    .callout()
+                                    .foregroundStyle(.gray)
+                            }
+                            .padding(.top, 32)
+                        }
+                    }
                 }
-                .padding(.top, 32)
+            } else {
+                List {
+                    Section {
+                        DatePicker("Date", selection: $date, displayedComponents: .date)
+                            .secondary()
+                        
+                        DatePicker("Time", selection: $date, displayedComponents: .hourAndMinute)
+                            .secondary()
+                        
+                        HStack {
+                            Text("Weight")
+                                .secondary()
+                            
+                            Spacer()
+                            
+                            TextField("", text: $enteredWeight)
+                                .focused($isWeightFieldFocused)
+                                .multilineTextAlignment(.trailing)
+                                .keyboardType(.decimalPad)
+                            
+                            Text("KG")
+                        }
+                    }
+                }
+                .task {
+                    await Task.yield()
+                    isWeightFieldFocused = true
+                }
             }
         }
         .navigationTitle("Weight")
         .navigationBarTitleDisplayMode(.inline)
-        .padding(15)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Button(role: .destructive) {
@@ -38,13 +86,31 @@ struct LogWeightSheet: View {
             
             ToolbarItem(placement: .topBarTrailing) {
                 SFButton("checkmark", action: saveRecord)
+                    .disabled(saveDisabled)
             }
         }
     }
     
     private func saveRecord() {
-        vm.writeWeight(value: Double(selectedValue))
+        let value: Double
+        
+        if #available(iOS 18, *) {
+            value = Double(selectedValue)
+        } else {
+            guard let fallbackWeight else { return }
+            value = fallbackWeight
+        }
+        
+        vm.writeWeight(value: value, date: date)
         dismiss()
+    }
+    
+    private var saveDisabled: Bool {
+        if #available(iOS 18, *) {
+            false
+        } else {
+            fallbackWeight == nil
+        }
     }
 }
 

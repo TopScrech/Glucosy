4c4aa5a0433f69ea633ede2c3f0c3dbbb96bc6c5
TopScrech <sergei_saliukov@icloud.com>
2026-05-24 23:11:57 +0200

    refactor

--- Widgets/GlucosyWidgets.swift
+++ Widgets/GlucosyWidgets.swift
@@ -1,41 +1,6 @@
 import SwiftUI
 import WidgetKit
 
-struct Provider: AppIntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
-    }
-    
-    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: configuration)
-    }
-    
-    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
-        var entries: [SimpleEntry] = []
-        
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date
-        let currentDate = Date()
-        
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-        
-        return Timeline(entries: entries, policy: .atEnd)
-    }
-    
-    //    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
-    //        // Generate a list containing the contexts this widget is relevant in
-    //    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationAppIntent
-}
-
 struct GlucosyWidgetsEntryView : View {
     private let entry: Provider.Entry
     
@@ -58,12 +23,8 @@ struct GlucosyWidgets: Widget {
     private let kind = "Glucosy_Widgets"
     
     var body: some WidgetConfiguration {
-        AppIntentConfiguration(
-            kind: kind,
-            intent: ConfigurationAppIntent.self,
-            provider: Provider()
-        ) { entry in
-            GlucosyWidgetsEntryView(entry)
+        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) {
+            GlucosyWidgetsEntryView($0)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
     }

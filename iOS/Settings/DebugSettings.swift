450d0d0594dfd9426121a9167d4f7c5300e42514
TopScrech <sergei_saliukov@icloud.com>
2026-06-08 09:21:14 +0200

    Added app version to debug settings

--- iOS/Settings/DebugSettings.swift
+++ iOS/Settings/DebugSettings.swift
@@ -1,12 +1,20 @@
-import SwiftUI
+import ScrechKit
 
 struct DebugSettings: View {
     @EnvironmentObject private var store: ValueStore
     
+    private var version: String {
+        "v\(Bundle.version ?? "Unknown") (B\(Bundle.build ?? "Unknown"))"
+    }
+    
     var body: some View {
         List {
-            Toggle("Debug mode", isOn: $store.debugMode)
-                .foregroundStyle(.foreground)
+            Section {
+                LabeledContent("App Version", value: version)
+                
+                Toggle("Debug mode", isOn: $store.debugMode)
+                    .foregroundStyle(.foreground)
+            }
             
             Section {
                 Toggle("Hide Status Bar", isOn: $store.debugHideStatusBar)
